891c367d792d5f71c8ee87ff80a60cb2cddacdd3
Ivan_HAN <hyf1996@qq.com>
2020-10-25 09:57:02 +0800

    添加item和tag的sql方法

--- persistence/dao/items.go
+++ persistence/dao/items.go
@@ -6,15 +6,19 @@ import (
 )
 
 type Item struct {
-	Id         int64
-	Path       string
-	Alias      string
-	Type       int
-	UpdateDate int64
-	CreateDate int64
+	Id         int64  `json:"id"`
+	Path       string `json:"path"`
+	Alias      string `json:"alias"`
+	Type       int64  `json:"type"`
+	UpdateDate int64  `json:"update_date"`
+	CreateDate int64  `json:"create_date"`
+	Tags       []Tag  `json:"tags"`
 }
 
-func CreateItem(tx sql.Tx, item Item) error {
+/**
+添加item
+*/
+func CreateItem(tx *sql.DB, item Item) error {
 	query := buildCreateItemSQL(item)
 	_, err := tx.Exec(query)
 	if err != nil {
@@ -23,8 +27,47 @@ func CreateItem(tx sql.Tx, item Item) error {
 	return nil
 }
 
-func GetItems() error {
+/**
+获取item列表
+*/
+func GetItems(db *sql.DB) ([]Item, error) {
+	//list := []Item{}
+	query := buildGetItemSQL()
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var ret []Item
+	for rows.Next() {
+		var item Item
+		if err := rows.Scan(&item.Id, &item.Path, &item.Alias, &item.Type, &item.CreateDate, &item.UpdateDate); err != nil {
+			return nil, err
+		}
+		ret = append(ret, item)
+	}
+	for _, item := range ret {
+		tags, err := GetTagsByItem(db, item.Id)
+		if err != nil {
+			continue
+		}
+		item.Tags = tags
+	}
+	return []Item{}, nil
+}
+
+/**
+为item设置tag
+*/
+func SetItemTag(id int64) error {
 	return nil
+}
+
+/**
+根据tag获取item
+*/
+func GetItemsByTags(id []int64) ([]Item, error) {
+	return nil, nil
 }
 
 func buildCreateItemSQL(item Item) string {
@@ -37,6 +80,6 @@ VALUES
 	return sql
 }
 
-func GetItemSQL() string {
+func buildGetItemSQL() string {
 	return `SELECT items.* FROM items`
 }
--- persistence/dao/tags.go
+++ persistence/dao/tags.go
@@ -1,8 +1,59 @@
 package dao
 
+import "database/sql"
+
 type Tag struct {
-	Id         int64
-	Title      string
-	CreateDate int64
-	UpdateDate int64
+	Id         int64  `json:"id"`
+	Title      string `json:"title"`
+	CreateDate int64  `json:"create_date"`
+	UpdateDate int64  `json:"update_date"`
+}
+
+/**
+获取标签列表
+*/
+func GetTags() ([]Tag, error) {
+	return nil, nil
+}
+
+/**
+添加标签
+*/
+func CreateTag() error {
+	return nil
+}
+
+/**
+获取item对应的tags
+*/
+func GetTagsByItem(db *sql.DB, itemId int64) ([]Tag, error) {
+	query := buildGetTagsByItem()
+	rows, err := db.Query(query, itemId)
+	if err != nil {
+		return nil, err
+	}
+	var ret []Tag
+	for rows.Next() {
+		var tag Tag
+		if err := rows.Scan(&tag.Id, &tag.Title); err != nil {
+			return nil, err
+		}
+		ret = append(ret, tag)
+	}
+	return nil, nil
+}
+
+func buildCreateTag() string {
+	return ""
+}
+
+func buildGetTagsByItem() string {
+	return `
+SELECT
+	tags.*
+FROM
+	tags
+	INNER JOIN res_tags_items ON res_tags_items.tag_id = tags.id 
+	AND res_tags_items.item_id = ?;
+`
 }
